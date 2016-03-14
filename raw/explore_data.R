841291d0456ee1ad4367b6c5d1cad7107d0c4174
mattsedlar <sedlarm@gmail.com>
2016-03-14 14:49:20 -0700

    Updated wordclouds with RColorBrewer

--- raw/explore_data.R
+++ raw/explore_data.R
@@ -39,27 +39,38 @@ BigramTokenizer <- function(x) {
   unlist(lapply(ngrams(words(x),2),paste, collapse=" "), use.names = F)
 }
 
-# term document matrix
-tdm <- TermDocumentMatrix(c, control=list(tokenize=BigramTokenizer))
+# term document matrix, 1-gram and 2-gram
+tdm.1g <- TermDocumentMatrix(c)
+tdm.2g <- TermDocumentMatrix(c, control=list(tokenize=BigramTokenizer))
 
 # removing sparse terms
-tdm.common40 <- removeSparseTerms(tdm,.4)
+tdm.1g.common20 <- removeSparseTerms(tdm.1g,.20)
+tdm.2g.common20 <- removeSparseTerms(tdm.2g,.20)
 
 # finding most-common terms
-tdm.common40.matrix <- as.matrix(tdm.common40)
+tdm.1g.common20.matrix <- as.matrix(tdm.1g.common20)
+tdm.2g.common20.matrix <- as.matrix(tdm.2g.common20)
 
-frequency <- rowSums(tdm.common40.matrix)
+frequency.1g <- rowSums(tdm.1g.common20.matrix)
+frequency.2g <- rowSums(tdm.2g.common20.matrix)
 
-frequency <- sort(frequency, decreasing = T)
+frequency.1g <- sort(frequency.1g, decreasing = T)
+frequency.2g <- sort(frequency.2g, decreasing = T)
 
-head(frequency)
-
-# wordcloud
+# wordclouds
 
 library(wordcloud)
+library(RColorBrewer)
 
-words <- names(frequency)
+pal <- brewer.pal(3,"Greys")
 
-wordcloud(words[1:100], frequency[1:100], scale=c(10,.75))
+words.1g <- names(frequency.1g)
+words.2g <- names(frequency.2g)
 
-## Ngrams
+png("images/wordcloud-1g.png",width=480, height=480)
+wordcloud(words.1g[1:200], frequency.1g[1:200], scale=c(10,.5),random.order = F,colors=pal)
+dev.off()
+
+png("images/wordcloud-2g.png",width=480, height=480)
+wordcloud(words.2g[1:200], frequency.2g[1:200], scale=c(10,.5),random.order = F,colors=pal)
+dev.off()
