ed46ae73df0a07c1f680f3f7434ec623efbd7602
mattsedlar <sedlarm@gmail.com>
2016-03-31 20:20:50 -0700

    tolower() on text entries, trigram statements

--- raw/new-model.R
+++ raw/new-model.R
@@ -9,6 +9,8 @@ df.3g <- fread("data/final/threegrams.csv")
 df.4g <- fread("data/final/fourgrams.csv")
 
 bea <- function(x) {
+  # convert to lowercase
+  x <- tolower(x)
   # unigram
   if(!grepl(" ",x) >= 1) {
     search <- subset(df.1g,grepl(paste("^",x,sep=""),token))
@@ -17,8 +19,9 @@ bea <- function(x) {
   
   # everything else
   else {
+    
     temp <- strsplit(x," ")
-    spaces <- length(gregexpr("\\W",x)[[1]])
+    spaces <- length(gregexpr("\\s",x)[[1]])
     
     # bigram
     if(spaces == 1){
@@ -38,7 +41,6 @@ bea <- function(x) {
         print(result[[1]][2])
       } else {
         # print("backoff 1")
-        prob_a <- subset(df.1g,grepl(paste("^",temp[[1]][2],sep=""),token))[1,]
         unigrams <- subset(df.1g,grepl(paste("^",temp[[1]][2],sep=""),token))
         unigrams$conditional <- 0.4 * 0.4 * (as.numeric(unigrams$prob))
         unigrams <- unigrams[order(-conditional),]
@@ -48,6 +50,44 @@ bea <- function(x) {
     }
     
     # trigrams
+    if(spaces == 2){
+      
+      print("trigrams")
+      # probability of first two words
+      prob_a <- subset(df.2g,grepl(paste("^",
+                                         paste(temp[[1]][1:2], collapse=" ")
+                                         ,sep=""),token))[1,]
+      # trigram with probability
+      trigrams <- subset(df.3g,grepl(paste("^",x,sep=""),token))
+      
+      # check if they exist
+      if(!is.na(prob_a$token) & length(trigrams$token)>0) {
+        # conditional probability of trigrams with backoff smoothing
+        trigrams$conditional <- 0.4 * (as.numeric(trigrams$prob)/prob_a$prob)
+        trigrams <- trigrams[order(-conditional),]
+        result <- strsplit(head(trigrams$token,1)," ")
+        print(result[[1]][3])
+      } else {
+        print("backoff 1")
+        prob_a <- subset(df.1g,grepl(paste("^",temp[[1]][2],sep=""),token))[1,]
+        bigrams <- subset(df.2g,grepl(paste("^",
+                                            paste(temp[[1]][2:3], collapse=" "),
+                                            sep=""),token))
+        if(!is.na(prob_a$token) & length(bigrams$token)>0) {        
+          bigrams$conditional <- 0.4 * 0.4 * (as.numeric(bigrams$prob))
+          bigrams <- bigrams[order(-conditional),]
+          result <- strsplit(head(bigrams$token,1)," ")
+          print(result[[1]][2])
+        } else {
+          print("backoff 2")
+          unigrams <- subset(df.1g,grepl(paste("^",temp[[1]][3],sep=""),token))
+          unigrams$conditional <- 0.4 * 0.4 * (as.numeric(unigrams$prob))
+          unigrams <- unigrams[order(-conditional),]
+          result <- head(unigrams$token,1)
+          print(result)
+        }
+      }
+    }
     
   }        
 }
